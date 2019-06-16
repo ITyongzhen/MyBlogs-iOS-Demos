1396c8886f51499ba5994e26487d6956830f3f89
ITyongzhen <eagle_01yz@163.com>
2019-06-16 13:14:19 +0800

    update

---
+++ RunLoop/Interview01-线程保活/Interview03-线程保活/ViewController.m
@@ -0,0 +1,50 @@
+
+
+#import "ViewController.h"
+
+/** YZThread **/
+@interface YZThread : NSThread
+@end
+@implementation YZThread
+- (void)dealloc
+{
+    NSLog(@"%s", __func__);
+}
+@end
+
+@interface ViewController ()
+@property (strong, nonatomic) YZThread *thread;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.thread = [[YZThread alloc] initWithTarget:self selector:@selector(run) object:nil];
+    [self.thread start];
+}
+- (void)run {
+    NSLog(@"%s %@", __func__, [NSThread currentThread]);
+    // 往RunLoop里面添加Source\Timer\Observer
+    [[NSRunLoop currentRunLoop] addPort:[[NSPort alloc] init] forMode:NSDefaultRunLoopMode];
+    [[NSRunLoop currentRunLoop] run];
+    NSLog(@"%s ----end----", __func__);
+}
+// 这个方法的目的：线程保活
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"点击屏幕");
+    [self performSelector:@selector(test) onThread:self.thread withObject:nil waitUntilDone:NO];
+}
+// 子线程需要执行的任务
+- (void)test
+{
+    NSLog(@"%s %@", __func__, [NSThread currentThread]);
+}
+//// 这个方法：线程不保活
+//- (void)run {
+//    NSLog(@"%s %@", __func__, [NSThread currentThread]);
+//
+//}
+@end
---
+++ RunLoop/Interview02-线程保活/Interview03-线程保活/ViewController.m
@@ -0,0 +1,84 @@
+//
+//  ViewController.m
+//  Interview03-线程保活
+//
+//  Created by MJ Lee on 2018/6/3.
+//  Copyright © 2018年 MJ Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "MJThread.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) MJThread *thread;
+@property (assign, nonatomic, getter=isStoped) BOOL stopped;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    __weak typeof(self) weakSelf = self;
+    
+    self.stopped = NO;
+    
+    self.thread = [[MJThread alloc] initWithBlock:^{
+        NSLog(@"%@----begin----", [NSThread currentThread]);
+        
+        // 往RunLoop里面添加Source\Timer\Observer
+        [[NSRunLoop currentRunLoop] addPort:[[NSPort alloc] init] forMode:NSDefaultRunLoopMode];
+        // 这个方法会不能退出循环
+//        [[NSRunLoop currentRunLoop] run];
+        while (!weakSelf.isStoped) {
+            [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+        }
+        
+        NSLog(@"%@----end----", [NSThread currentThread]);
+        
+        // NSRunLoop的run方法是无法停止的，它专门用于开启一个永不销毁的线程（NSRunLoop）
+        //        [[NSRunLoop currentRunLoop] run];
+        /*
+         it runs the receiver in the NSDefaultRunLoopMode by repeatedly invoking runMode:beforeDate:.
+         In other words, this method effectively begins an infinite loop that processes data from the run loop’s input sources and timers
+         */
+        
+    }];
+    [self.thread start];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self performSelector:@selector(test) onThread:self.thread withObject:nil waitUntilDone:NO];
+}
+
+// 子线程需要执行的任务
+- (void)test
+{
+    NSLog(@"%s %@", __func__, [NSThread currentThread]);
+}
+
+- (IBAction)stop {
+    // 在子线程调用stop
+    [self performSelector:@selector(stopThread) onThread:self.thread withObject:nil waitUntilDone:NO];
+}
+
+// 用于停止子线程的RunLoop
+- (void)stopThread
+{
+    // 设置标记为NO
+    self.stopped = YES;
+    
+    // 停止RunLoop
+    CFRunLoopStop(CFRunLoopGetCurrent());
+    NSLog(@"%s %@", __func__, [NSThread currentThread]);
+}
+
+- (void)dealloc
+{
+    NSLog(@"%s", __func__);
+    
+//    [self stop];
+}
+
+@end
